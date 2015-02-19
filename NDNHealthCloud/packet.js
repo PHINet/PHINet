7928bc898238d42d99eee70415fff3523e3e1128
s,rc <seales.rc@gmail.com>
2015-02-18 18:21:18 -0600

    .

---
+++ NDNHealthCloud/packet.js
@@ -0,0 +1,11 @@
+
+/** 
+ * File contains code for the data-object  
+ * used to handle patient information
+ **/
+
+
+exports.packet = function() {
+	// NOTE: contents will be returned when 
+	// other modules "require" this 
+}
---
+++ NDNHealthCloud/udp_comm.js
@@ -0,0 +1,10 @@
+/** 
+ * File contains code for the data-object  
+ * used to handle patient information
+ **/
+
+
+exports.udp_comm = function() {
+	// NOTE: contents will be returned when 
+	// other modules "require" this 
+}
